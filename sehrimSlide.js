4b1e33d70129011f0eab1859759a063854bf3a80
denizsimsekk <78348807+denizsimsekk@users.noreply.github.com>
2021-05-23 23:06:23 +0300

    Add files via upload

---
+++ sehrimSlide.js
@@ -0,0 +1,22 @@
+function sehirNufusu()
+{
+    document.getElementById("sehirNufusu").style.background="#58e482";
+    setTimeout(function(){ document.getElementById("sehirNufusu").style.background="#9fe29d"; }, 2000);
+}
+
+function antikKent()
+{
+    document.getElementById("antikKent").style.background="#58e482";
+    setTimeout(function(){ document.getElementById("antikKent").style.background="#9fe29d"; }, 2000);
+}
+
+function kanyonSatiri()
+{
+    document.getElementById("kanyonSatiri").style.background="#58e482";
+    setTimeout(function(){ document.getElementById("kanyonSatiri").style.background="#9fe29d"; }, 2000);
+}
+function marmarisMuzesi()
+{
+    document.getElementById("marmarisMuzesi").style.background="#58e482";
+    setTimeout(function(){ document.getElementById("marmarisMuzesi").style.background="#9fe29d"; }, 2000);
+}
